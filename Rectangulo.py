8b4eebb2b54f05189c2e8efff038bb723fb52406
feehx25 <49326819+feehx25@users.noreply.github.com>
2021-03-31 20:54:39 -0300

    Agrega el punto 3

--- Rectangulo.py
+++ Rectangulo.py
@@ -7,6 +7,8 @@ def pedirPerimetro():
 
     print(f"El perimetro de su rectangulo es {perimetro}")
 
+    cargarFunciones()
+
 
 
 def calcularPerimetro(base, altura):
@@ -20,6 +22,8 @@ def pedirArea():
     area = calcularArea(int(input("Ingrese la base del rectangulo ")),int(input("Ingrese la altura del rectangulo ")))
     print(f"El area de su rectangulo es {area}")
 
+    cargarFunciones()
+
 
 def calcularArea(base,altura):
 
@@ -27,8 +31,63 @@ def calcularArea(base,altura):
     return area
 
 
-pedirArea()
-#pedirPerimetro()
+def pedirAreaEjes():
+    print("Calcular el area de un rectangulo teniendo en cuenta los ejes x1, x2 , y1 e y2")
+    print("ingrese los valores para la coordenada x")
+    x1 = int(input("ingrese el valor de x1:"))
+    x2 = int(input("ingrese el valor de x2:"))
+    print("ingrese los valores para la coordenada y")
+    y1 = int(input("ingrese el valor de y1:"))
+    y2 = int(input("ingrese el valor de y2:"))
+    print(f"El area es {calcularAreaEjes(x1, x2, y1, y2)}")
+
+    cargarFunciones()
+
+
+def calcularAreaEjes(x1,x2,y1,y2):
+    
+    base = calcularDistancia(x1,x2)
+    altura = calcularDistancia(y1, y2)
+
+    area = base * altura
+
+    return area
+    
+def calcularDistancia(n1,n2):
+    distancia = 0
+    if abs(n1)> abs(n2):
+        distancia = abs(n1)-abs(n2)
+    else:
+        distancia = abs(n2)-abs(n1)
+    return distancia
+
+
+
+def cargarFunciones():
+    opcion = int(input("""
+
+    Seleccione la funcion a utilizar:"
+
+1.Calcular perimetro
+2.Calcular area
+3.Calcular area con ejes
+
+"""))
+    if opcion == 1:
+        pedirPerimetro()
+    elif opcion == 2:
+        pedirArea()
+    elif opcion == 3:
+        pedirAreaEjes()
+
+
+cargarFunciones()
+
+
+
+
+
+
 
 
     
