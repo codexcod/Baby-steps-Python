ee8817752b73cfc12e66fb3f2ffc325ad9fc1c00
feehx25 <49326819+feehx25@users.noreply.github.com>
2021-03-31 20:12:48 -0300

    Ahora podes pedir el area

--- Rectangulo.py
+++ Rectangulo.py
@@ -14,9 +14,21 @@ def calcularPerimetro(base, altura):
     perimetro = base * 2 + altura * 2
     return perimetro
 
+def pedirArea():
+
+    print("Calcular el area de un rectangulo")
+    area = calcularArea(int(input("Ingrese la base del rectangulo ")),int(input("Ingrese la altura del rectangulo ")))
+    print(f"El area de su rectangulo es {area}")
 
 
-pedirPerimetro()
+def calcularArea(base,altura):
+
+    area = base * altura
+    return area
+
+
+pedirArea()
+#pedirPerimetro()
 
 
     
