620d977a0711337e78c271b2119f53f2ebfdb4ac
codexcod <nunezjoac@gmail.com>
2021-04-10 11:22:24 -0300

    Ejercicio 10 terminado

--- ejercicio10.py
+++ ejercicio10.py
@@ -6,5 +6,19 @@ x=(random.randrange(50))
 intentos = 0
 numero = 0
 
+while not numero == x:
+    numero = int(input("Ingrese un numero"))
+    
+    if numero > x:
+        intentos += 1
+        print("El numero es mas chico")
 
+    elif numero < x:
+        intentos += 1
+        print ("El numero es mas grande")
+        
+    elif numero == x:
+        print(f"El numero es correcto, acertaste en el intento {intentos}")
+        intentos = 0
+        break
    
