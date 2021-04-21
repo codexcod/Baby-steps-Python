afaaa180932254c5f79aee4007d5d689c959b4ae
codexcod <nunezjoac@gmail.com>
2021-04-21 19:30:33 -0300

    Comenzando ejercicio 18

---
+++ ejercico18.py
@@ -0,0 +1,7 @@
+"""18. Inversión de listas
+1. Realizar una función que, dada una lista, devuelva una nueva lista cuyo contenido sea
+igual a la original pero invertida. Así, dada la lista ['Di', 'buen', 'día', 'a', 'papa'], deberá
+devolver ['papa', 'a', 'día', 'buen', 'Di'].
+2. Realizar otra función que invierta la lista, pero en lugar de devolver una nueva,
+modifique la lista dada para invertirla, sin usar listas auxiliares"""
+
