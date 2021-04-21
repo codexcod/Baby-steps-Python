6c3a336c7e6fe77537bdf3afbf1e649e0dd392e5
codexcod <nunezjoac@gmail.com>
2021-04-21 20:25:11 -0300

    Terminado ejercicio 18

--- ejercico18.py
+++ ejercico18.py
@@ -5,3 +5,21 @@ devolver ['papa', 'a', 'día', 'buen', 'Di'].
 2. Realizar otra función que invierta la lista, pero en lugar de devolver una nueva,
 modifique la lista dada para invertirla, sin usar listas auxiliares"""
 
+def invertirListaConAuxiliar(lista):
+    """Dada una lista devuelve una nueva lista com la primera lista invertida"""
+    resultado = []
+    for i in range(len(lista)-1,-1,-1):
+        resultado.append(lista[i])
+    return resultado
+
+def invertirLista(lista):
+    """Dada una lista devuelve la lista invertida"""
+    cantidad = len(lista)
+    for i in range(cantidad):
+        lista.append(lista[cantidad-i-1])
+        lista.remove(lista[cantidad-i-1])
+        
+    return lista
+
+
+
