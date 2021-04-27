54f4878d850752139be73558e392379c210ee744
codexcod <nunezjoac@gmail.com>
2021-04-27 19:42:42 -0300

    documento ejercicio 10

--- ejercicio10.py
+++ ejercicio10.py
@@ -1,5 +1,10 @@
 import random   
 from random import randrange
+
+"""10. Utilizando la función randrange del módulo random, escribir un programa que tenga un
+número aleatorio secreto, y luego permita al usuario ingresar números y le indique si son
+menores o mayores que el número a adivinar, hasta que el usuario ingrese el número
+correcto. """
            
 x=(random.randrange(50))
 
@@ -7,6 +12,8 @@ intentos = 0
 numero = 0
 
 while not numero == x:
+    """Mientras el numero no sea correco el jugador puede seguir intentando"""
+
     numero = int(input("Ingrese un numero"))
     
     if numero > x:
