471fe8caf4aad690395017349ac448a6b19cd0ef
codexcod <nunezjoac@gmail.com>
2021-04-27 19:44:46 -0300

    documento ejercicio 11

--- ejercicio11.py
+++ ejercicio11.py
@@ -1,3 +1,10 @@
+""" 11. Escribir una función que dada la cantidad de ejercicios de un examen, y el porcentaje
+necesario de ejercicios bien resueltos necesario para aprobar dicho examen, revise un grupo
+de exámenes. Para ello, en cada paso debe preguntar la cantidad de ejercicios resueltos por
+el alumno, indicando con un valor centinela que no hay más exámenes a revisar. Debe
+mostrar por pantalla el porcentaje correspondiente a la cantidad de ejercicios resueltos
+respecto a la cantidad de ejercicios del examen y una leyenda que indique si aprobó o no """
+
 def calcularNotaMinima(ejercicios, porcentaje):
     """Calcula la nota minama para aprobar la prueba teniendo en cuenta la cantidad de ejercicios y el porcentaje minimo para aprobar"""
     notaMinima = (porcentaje * ejercicios) / 100
