75c64ef4aa873340f27f8e4201ae828702da555b
codexcod <nunezjoac@gmail.com>
2021-04-27 19:29:30 -0300

    documento ejercicio 6

--- ejercicio6.py
+++ ejercicio6.py
@@ -1,4 +1,11 @@
+"""6. Escribir una función que, dados cuatro números, devuelva el mayor producto de dos de
+ellos. Por ejemplo, si recibe los números 1, 5, -2, -4 debe devolver 8, que es el producto más
+grande que se puede obtener entre ellos (8 = −2 × −4). """
+
+
 def calcularMayorProducto(n1,n2,n3,n4):
+    """Dados 4 numeros devuelve el mayor prducto entre 2 de esos numeros"""
+
     mayorProducto = 0
 
     for i in (n2,n3,n4):
@@ -18,8 +25,10 @@ def calcularMayorProducto(n1,n2,n3,n4):
 
 
 def pedirNumeros():
+    """Funcion que printea la funcion anterior"""
+
     print("Calcular el mayor productos dados 4 numeros")
     resultado = calcularMayorProducto(int(input("Ingrese el primer numero : ")),int(input("Ingrese el segundo numero : ")),int(input("Ingrese el tercer numero : ")),int(input("Ingrese el cuarto numero : ")))
     print(f"El mayor producto es : {resultado}")
 
-pedirNumeros()
+
