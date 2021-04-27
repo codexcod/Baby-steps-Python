33f2f587fc23fea14c3bca407672782320fad7ad
codexcod <nunezjoac@gmail.com>
2021-04-27 18:10:09 -0300

    documento ejercicio 3

--- ejercicio3.py
+++ ejercicio3.py
@@ -1,6 +1,9 @@
+"""3. Implementar un algoritmo (en una o más funciones) que, dado un número entero n, permita
+calcular su factorial."""
+
 def calcularFactorial(n):
 
-    #Calcula el factorial de un numero n#
+    """Calcula el factorial de un numero n"""
 
     resultado = n
     for i in range(1 , n):
