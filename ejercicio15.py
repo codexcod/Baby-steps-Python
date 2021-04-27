4bc96c0859895e2196630a73f303201f250fa512
codexcod <nunezjoac@gmail.com>
2021-04-27 19:52:46 -0300

    documento ejercicio 15

--- ejercicio15.py
+++ ejercicio15.py
@@ -1,5 +1,14 @@
+"""15. Escribir una función que dada una cadena de caracteres, devuelva:
+1. La primera letra de cada palabra. Por ejemplo, si recibe 'Universal Serial Bus' debe
+devolver 'USB'.
+2. Dicha cadena con la primera letra de cada palabra en mayúsculas. Por ejemplo, si recibe
+'república argentina' debe devolver 'República Argentina'.
+
+3. Las palabras que comiencen con la letra ‘A’. Por ejemplo, si recibe 'Antes de ayer' debe
+devolver 'Antes ayer'"""
 
 def mostrarSiglasDeCadena(cadena):
+    """Convierte una cadena de texto en siglas usando las primeras letras"""
     palabras = cadena.split(" ")
     resultado=""
 
@@ -9,6 +18,7 @@ def mostrarSiglasDeCadena(cadena):
     return resultado.upper()
 
 def mostrarMayusculaAlPrimero(cadena):
+    """Convierte a todas las primeras letras de cada palabra en mayuscula"""
     palabras = cadena.split(" ")
 
     resultado = ""
@@ -20,6 +30,7 @@ def mostrarMayusculaAlPrimero(cadena):
     return resultado
 
 def mostrarPalabrasConA(cadena):
+    """Solo muestra las palabras de la cadena que empiezen con a"""
     palabras = cadena.split(" ")
 
     resultado = ""
@@ -35,7 +46,7 @@ def mostrarPalabrasConA(cadena):
 
     return resultado
 
-print(mostrarSiglasDeCadena("ayer nomas ayer hola algo"))
+
 
 
 
