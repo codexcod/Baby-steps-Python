09cc792bca852fa27d18f4f3f8cbb2009120d397
codexcod <nunezjoac@gmail.com>
2021-04-27 19:36:21 -0300

    documento ejercicio 8

--- ejercicio6.py
+++ ejercicio6.py
@@ -1,6 +1,6 @@
 """6. Escribir una función que, dados cuatro números, devuelva el mayor producto de dos de
 ellos. Por ejemplo, si recibe los números 1, 5, -2, -4 debe devolver 8, que es el producto más
-grande que se puede obtener entre ellos (8 = −2 × −4). """
+grande que se puede obtener entre ellos (8 = −2 × −4) """
 
 
 def calcularMayorProducto(n1,n2,n3,n4):
--- ejercicio8.py
+++ ejercicio8.py
@@ -1,12 +1,15 @@
-
+"""
+8. (Puede ser difícil) Escribir un programa que reciba como entrada un entero representando un
+año (por ejemplo 751, 1999, o 2158), y muestre por pantalla el mismo año escrito en
+números romanos.
+"""
 
 romanos = [(1000, "M"), (900, "CM"), (500, "D"), (400, "CD"), (100, "C"), (90, "XC"),
            (50, "L"), (40, "XL"), (10, "X"), (9, "IX"), (5, "V"), (4, "IV"), (1, "I")]
 
 def pasarRomano(año):
- # Convierte el año ingresado en numeros romanos
+    """Dado un año normal la funcion lo convierte en romano"""
     resultado = ""
-
     while año > 0:
         for n, r in romanos:
             while año > n or año == n:
