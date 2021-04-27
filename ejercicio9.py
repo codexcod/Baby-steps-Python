4c43ffcc5c9717b5c9273167fac29a147039c1a1
codexcod <nunezjoac@gmail.com>
2021-04-27 19:40:15 -0300

    documento ejercicio 9

--- ejercicio9.py
+++ ejercicio9.py
@@ -1,9 +1,16 @@
 import time
 from time import sleep
+""" 9. Escribir un programa que contenga una contraseña inventada, que le pregunte al usuario la
+contraseña, y no le permita continuar hasta que la haya ingresado correctamente.
+1. Modificar el programa anterior para que solamente permita una cantidad fija de intentos.
+2. Modificar el programa anterior para que después de cada intento agregue una pausa cada
+vez mayor, utilizando la función sleep del módulo time.
+3. Modificar el programa anterior para que sea una función que devuelva si el usuario
+ingresó o no la contraseña correctamente, mediante un valor booleano (True o False). """
 
 def validarContraseña(contraseña):
-    CONTRASEÑA = "contraseña123@"
-    return contraseña == CONTRASEÑA
+    """Verifica que la contraseña sea correcta"""
+    return contraseña == "contraseña123@"
 
 
 
@@ -11,6 +18,7 @@ intentos = 3
 espera = 2 
 
 while intentos > 0:
+    """Mientras el jugador tenga intentos puede ingresar una contraseña"""
 
     contraseña_ingresada = input("Ingrese la contraseña ")
 
