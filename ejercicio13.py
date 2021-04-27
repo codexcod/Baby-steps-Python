7223fb551f5f5514039e4e5183c7f04cc334db1c
codexcod <nunezjoac@gmail.com>
2021-04-27 19:46:31 -0300

    documento ejercicio 13

--- ejercicio13.py
+++ ejercicio13.py
@@ -1,3 +1,7 @@
+"""13. Escribir un programa que compare dos strings y devuelva True si el primero es la versión
+capitalizada del segundo.
+Nota: Por ejemplo, la versión capitalizada de la palabra “programar” es
+“PROGRAMAR”."""
 
 def esCpaitalizada(palabra1,palabra2):
     """Compara si la primera palabra es la version capitalizada de la segunda"""
