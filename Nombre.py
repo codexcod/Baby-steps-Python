3f92915897e4fc389f6c7cae5d26741deeecbd62
codexcod <nunezjoac@gmail.com>
2021-05-04 22:04:16 -0300

    Documento y mejoro ejercicio 1.1

--- Nombre.py
+++ Nombre.py
@@ -1,10 +1,15 @@
+"""1. Escribir un programa que pregunte al usuario:
+1.Su nombre, y luego lo salude."""
 
 def pedirNombre(nombre):
+    """Recibe:
+            nombre:<string>
+        Deuvelve un saludo con el nombre dado"""
+    return(f"Hola {nombre}, como estas?")
 
-    print(f"Hola {nombre}, como estas?")
 
 
-pedirNombre(input("Ingrese su nombre "))
+print(pedirNombre(input("Ingrese su nombre ")))
 
 
 
