ecfa99ca880f46504a742a15c6a44323f44a9b3d
codexcod <nunezjoac@gmail.com>
2021-05-05 01:00:32 -0300

    Documento y mejoro ejercicio 11

--- ejercicio11.py
+++ ejercicio11.py
@@ -6,35 +6,50 @@ mostrar por pantalla el porcentaje correspondiente a la cantidad de ejercicios r
 respecto a la cantidad de ejercicios del examen y una leyenda que indique si aprobó o no """
 
 def calcularNotaMinima(ejercicios, porcentaje):
-    """Calcula la nota minama para aprobar la prueba teniendo en cuenta la cantidad de ejercicios y el porcentaje minimo para aprobar"""
+    """Recibe:
+                       ejercicios:<int>
+                       porcentaje: <int>/<float>
+
+                   Devuelve la nota minama para aprobar la prueba teniendo en cuenta la cantidad de ejercicios y el porcentaje minimo para aprobar"""
     notaMinima = (porcentaje * ejercicios) / 100
     return notaMinima
 
 def calcularNotaPorcentaje(nota, ejercicios):
-    """Calcula el porcentaje de la prueba aprobado teniendo en cuenta la nota y la cantidad de ejercicios"""
+    """Recibe:
+                           nota:<int>/<float>
+                           ejercicios: <int>
+
+                       Devuelve el porcentaje de la prueba aprobado teniendo en cuenta la nota y la cantidad de ejercicios"""
     notaPorcentaje = (nota * 100) / ejercicios
     return notaPorcentaje
 
-def aprobarPureba(cantidad_ejercicios,minimo_porcentaje,nota):
+def aprobarPureba(cantidadEjercicios,minimoPorcentaje,nota):
+    """Recibe:
+                               cantidadEjercicios:<int>
+                               minimoPorcentaje: <int>/<float>
+                               nota:<int>/<float>
+
+                           Devuelve el porcentaje de la prueba aprobado teniendo en cuenta la nota y la cantidad de ejercicios"""
+
     """Calcula si la prueba esta aprobada o no teniendo en cuenta su nota, el porcentaje necesario para aprobar y la cantidad de ejercicios"""
 
-    while cantidad_ejercicios <= 0:
-        cantidad_ejercicios = int(input("Ingrese la cantidad de ejercicios "))
-        if cantidad_ejercicios <= 0:
+    while cantidadEjercicios <= 0:
+        cantidadEjercicios = int(input("Ingrese la cantidad de ejercicios "))
+        if cantidadEjercicios <= 0:
             print("No puede ingresar una cantidad negativa o 0")
 
-    while minimo_porcentaje < 0 or minimo_porcentaje > 100:
-        minimo_porcentaje = int(input("Ingrese el porcentaje de ejercicios "))
-        if cantidad_ejercicios <= 0:
+    while minimoPorcentaje < 0 or minimoPorcentaje > 100:
+        minimoPorcentaje = float(input("Ingrese el porcentaje de ejercicios para aprobar "))
+        if cantidadEjercicios <= 0:
             print("Ingrese un porcentaje valido")
 
     while nota <= 0:
-        nota = int(input("Ingrese cuantos ejercicios correctos: "))
-        if nota > cantidad_ejercicios or nota < 0:
+        nota = float(input("Ingrese cuantos ejercicios correctos: "))
+        if nota > cantidadEjercicios or nota < 0:
             print("Ingrese una nota valida")
 
-    if nota >= calcularNotaMinima(cantidad_ejercicios, minimo_porcentaje):
-        print(f"El alumno aprobo con un {calcularNotaPorcentaje(nota, cantidad_ejercicios)}% de los ejercicios correctos")
-    elif nota < aprobarExamen(cantidad_ejercicios, minimo_porcentaje):
-        print(f"El alumno reprobo con un {calcularNotaPorcentaje(nota, cantidad_ejercicios)}% de los ejercicios correctos")
+    if nota >= calcularNotaMinima(cantidadEjercicios, minimoPorcentaje):
+        return(f"El alumno aprobo con un {calcularNotaPorcentaje(nota, cantidadEjercicios)}% de los ejercicios correctos")
+    elif nota < aprobarExamen(cantidadEjercicios, minimoPorcentaje):
+        return(f"El alumno reprobo con un {calcularNotaPorcentaje(nota, cantidadEjercicios)}% de los ejercicios correctos")
 
