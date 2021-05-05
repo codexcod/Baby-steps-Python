7455c96ee3a8f21c92d5154df0478d44cf7b5ada
codexcod <nunezjoac@gmail.com>
2021-05-05 01:02:23 -0300

    Documento y mejoro ejercicio 11

--- ejercicio12.py
+++ ejercicio12.py
@@ -1,28 +1,19 @@
-class Vocales:
-    """ queria probar como se hace una clase"""
-    a = 0
-    e=0
-    i=0
-    o=0
-    u=0
-
 
 palabra = input("Ingrese la palbara para saber cauntas vocales tiene ")
 
-vocales = Vocales()
 
-vocales.a = palabra.count("a")
-vocales.e = palabra.count("e")
-vocales.i = palabra.count("i")
-vocales.o = palabra.count("o")
-vocales.u = palabra.count("u")
+a = palabra.count("a")
+e = palabra.count("e")
+i = palabra.count("i")
+o = palabra.count("o")
+u = palabra.count("u")
 
 
 print(f"""
-A:{vocales.a}  
-E:{vocales.e}   
-O:{vocales.o}
-I:{vocales.i}
-U:{vocales.u}
+A:{a}  
+E:{e}   
+O:{o}
+I:{i}
+U:{u}
 
 """)
