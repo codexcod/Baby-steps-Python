623f20cd827ebafb878d6a13c7fbcf8f0d2fe7b3
codexcod <nunezjoac@gmail.com>
2021-05-05 00:46:54 -0300

    Documento y mejoro ejercicio 8

--- ejercicio8.py
+++ ejercicio8.py
@@ -8,7 +8,11 @@ romanos = [(1000, "M"), (900, "CM"), (500, "D"), (400, "CD"), (100, "C"), (90, "
            (50, "L"), (40, "XL"), (10, "X"), (9, "IX"), (5, "V"), (4, "IV"), (1, "I")]
 
 def pasarRomano(año):
-    """Dado un año normal la funcion lo convierte en romano"""
+    """Recibe:
+                año:<int>
+
+               Devuelve el año dado en numero romanos"""
+
     resultado = ""
     while año > 0:
         for n, r in romanos:
@@ -17,10 +21,14 @@ def pasarRomano(año):
                 año = año - n
     return resultado
 
-año=int(input("Ingrese un año : "))
-if año < 0:
-    "Ingreso un año no valido"
-elif año == 0:
-    "No se epuede representar el 0"
-else: 
-    print(pasarRomano(año))
+
+año=input("Ingrese un año : ")
+if (str(año)).isdigit():
+    if int(año) < 0:
+        "Ingreso un año no valido"
+    elif int(año) == 0:
+        "No se epuede representar el 0"
+    else:
+        print(pasarRomano(int(año)))
+else:
+    print("Ingrese un numero correccto")
